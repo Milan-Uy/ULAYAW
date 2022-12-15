d1399a4a877e22bb6ad48fc71ff7290e34d1840c
Milan-Uy <uymillano@gmail.com>
2022-12-15 18:37:39 +0800

    fix welcome user and added redirect to reservation summary when logging in

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const navigate = () => {
       window.location.replace("/c-reg-code.html");
     } else {
       if (localStorage.getItem("user_role") === "Guest") {
+        // if (sessionStorage.getItem("hasVisited")) {
+        //   window.location.replace("/reservationdetails.html");
+        // } else {
         window.location.replace("/c-profile.html");
+        // }
       } else if (localStorage.getItem("user_role") === "Admin") {
         window.location.replace("/a-profile.html");
       } else if (localStorage.getItem("user_role") === "Manager" || localStorage.getItem("user_role") === "Marketing Specialist") {
